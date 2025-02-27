37136d86a90dccf032c04e32e364457ae5520bd2
IraMonasteretska <44293548+IraMonasteretska@users.noreply.github.com>
2025-02-27 19:05:14 +0200

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -1,101 +1,136 @@
 
 $(document).ready(function () {
 
-    // burger-menu 
-    $('.burger-btn').on('click', function () {
-        $(this).toggleClass('active');
-        $('.mobile-menu').toggleClass('active');
-        $('body').toggleClass('hidden');
+    // accordion info section 
+     if ($(".info .more-btn").length) {
+        $('.more-btn').on('click', function () {
+            $(this).toggleClass('open');
+            $('.hidden-text').slideToggle(700);
+        });
+    }
+    // scroll to video poster
+    var lastScrollTop = 0;
+
+    $(".poster").on("click", function () {
+        lastScrollTop = $(window).scrollTop();
     });
 
-    // mobile language 
-    $('.mobile__lang').on('click', function () {
-        $('.mobile__lang ul').slideToggle();
+    $(document).on("click", "#fancybox-close", function () {
+        setTimeout(function () {
+            $("html, body").scrollTop(lastScrollTop);
+        }, 500);
     });
+    // burger-menu 
+    if ($(".burger-btn").length) {
+        $('.burger-btn').on('click', function () {
+            $(this).toggleClass('active');
+            $('.mobile-menu').toggleClass('active');
+            $('body').toggleClass('hidden');
+        });
+
+        $('.mobile__button').on('click', function () {
+            $('.mobile-menu').removeClass('active');
+            $('body').removeClass('hidden');
+        });
+
+    }
+
+    // mobile language 
+    if ($(".mobile__lang").length) {
+        $('.mobile__lang').on('click', function () {
+            $('.mobile__lang ul').slideToggle();
+        });
+    }
 
 
     // logotypes carousel 
-    var swiper = new Swiper(".logotypes__carousel", {
-        slidesPerView: "auto",
-        spaceBetween: 16,
-        loop: true,
-        speed: 3000,
-        autoplay: {
-            delay: 0,
-        },
-    });
+    if ($(".logotypes__carousel").length) {
+        var swiper = new Swiper(".logotypes__carousel", {
+            slidesPerView: "auto",
+            spaceBetween: 16,
+            loop: true,
+            speed: 3000,
+            autoplay: {
+                delay: 0,
+            },
+        });
+    }
 
     // clients carousel
-    var swiper = new Swiper(".clients__carousel", {
-        slidesPerView: 4,
-        loop: true,
-        speed: 1500,
-        autoplay: {
-            delay: 800,
-        },
-        breakpoints: {
-            320: {
-                slidesPerView: 1,
+    if ($(".clients__carousel").length) {
+        var swiper = new Swiper(".clients__carousel", {
+            slidesPerView: 4,
+            loop: true,
+            speed: 1500,
+            autoplay: {
+                delay: 800,
             },
-            576: {
-                slidesPerView: 2,
+            breakpoints: {
+                320: {
+                    slidesPerView: 1,
+                },
+                576: {
+                    slidesPerView: 2,
+                },
+                992: {
+                    slidesPerView: 3,
+                },
+                1200: {
+                    slidesPerView: 4,
+                },
             },
-            992: {
-                slidesPerView: 3,
-            },
-            1200: {
-                slidesPerView: 4,
-            },
-        },
-    });
+        });
+    }
 
     //   parallax 
-    $(window).on("scroll", function () {
-        let scrollPos = $(window).scrollTop();
-        let scaleIndex = 1 + (scrollPos * 0.0002);
+    if ($(".bg-image").length) {
+        $(window).on("scroll", function () {
+            let scrollPos = $(window).scrollTop();
+            let scaleIndex = 1 + (scrollPos * 0.0002);
 
-        $(".bg-image img").css("transform", `scaleX(${scaleIndex})`);
-        $(".frame").css("transform", `translateY(-${scrollPos * 0.25}px)`);
-    });
+            $(".bg-image img").css("transform", `scaleX(${scaleIndex})`);
+            $(".frame").css("transform", `translateY(-${scrollPos * 0.25}px)`);
+        });
+    }
 
 
     //  advantages layout animation
-    if ($(".advantages").length) {
+    // if ($(".advantages").length) {
 
-        var cards = gsap.utils.toArray(".advantages__block");
+    //     var cards = gsap.utils.toArray(".advantages__block");
 
-        ScrollTrigger.create({
-            trigger: cards[0],
-            start: "center center"
-        });
+    //     ScrollTrigger.create({
+    //         trigger: cards[0],
+    //         start: "center center"
+    //     });
 
-        var lastCardTrigger = ScrollTrigger.create({
-            trigger: cards[cards.length - 1],
-            start: "center center"
-        });
+    //     var lastCardTrigger = ScrollTrigger.create({
+    //         trigger: cards[cards.length - 1],
+    //         start: "center center"
+    //     });
 
-        cards.forEach(function (card, index) {
-            var scaleValue = 1 - 0.02 * (cards.length - index);
+    //     cards.forEach(function (card, index) {
+    //         var scaleValue = 1 - 0.02 * (cards.length - index);
 
-            var animation = gsap.to(card, {
-                scale: scaleValue,
-                "transform-origin": '50% ' + (- lastCardTrigger.start * 0.15)
-            });
+    //         var animation = gsap.to(card, {
+    //             scale: scaleValue,
+    //             "transform-origin": '50% ' + (- lastCardTrigger.start * 0.15)
+    //         });
 
-            ScrollTrigger.create({
-                trigger: card,
-                start: "center center",
-                end: function () {
-                    return lastCardTrigger.start + 0;
-                },
-                pin: true,
-                pinSpacing: false,
-                ease: "none",
-                animation: animation,
-                toggleActions: "restart none none reverse"
-            });
-        });
-    }
+    //         ScrollTrigger.create({
+    //             trigger: card,
+    //             start: "center center",
+    //             end: function () {
+    //                 return lastCardTrigger.start + 0;
+    //             },
+    //             pin: true,
+    //             pinSpacing: false,
+    //             ease: "none",
+    //             animation: animation,
+    //             toggleActions: "restart none none reverse"
+    //         });
+    //     });
+    // }
 
 
     //  ways layout animation
@@ -114,11 +149,11 @@ $(document).ready(function () {
         });
 
         blocks.forEach(function (block, index) {
-            var scaleValue = 1 - 0.002 * (blocks.length - index);
+            var scaleValue = 0.97;
 
             var animation = gsap.to(block, {
                 scale: scaleValue,
-                "transform-origin": '50% ' + (- lastBlockTrigger.start * 0.5)
+                "transform-origin": '50% ' + "0%"
             });
 
             ScrollTrigger.create({
